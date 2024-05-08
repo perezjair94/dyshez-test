35e7a604335d0af21cbeaf0352864e80f340d4fb
Jair Pérez <oquendojair10@gmail.com>
2024-05-08 18:21:27 -0500

    feat(orders): fetch orders

--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -1,10 +1,21 @@
 import { Order } from '@/app/lib/definitions';
 import { createClient } from '@/app/lib/supabase/server';
 import Table from '@/app/ui/orders/table';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
   const supabase = createClient();
+
   const { data: orders } = await supabase.from('orders').select();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect('/login');
+  }
+
   return (
     <div className="flex-1 p-[20px] px-[32px]">
       <h1 className="mb-[40px] text-[1.5rem] font-bold">Orders</h1>
