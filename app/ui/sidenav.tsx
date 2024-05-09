2e1531400990192a9f67c0d2d7511a8c9ddd0349
Jair Pérez <oquendojair10@gmail.com>
2024-05-09 13:30:00 -0500

    feat: add avatarimage

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
 import NavLinks from './nav-links';
 
 export default function Sidenav() {
   return (
     <div className="sticky top-0 h-screen w-[56px] border-r border-night/10 bg-white py-[20px]">
-      <div className="mx-auto mb-[40px] h-[40px] w-[40px] rounded-full bg-dogwoose-rose" />
+      <div className="mx-auto mb-[40px] h-[40px] w-[40px] rounded-full bg-dogwoose-rose/40">
+        <Image src="/avatar.png" width={40} height={40} alt="avatar" />
+      </div>
       <nav className="w-[56px]">
         <NavLinks />
       </nav>
